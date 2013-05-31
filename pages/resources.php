ed52d56bfbff537e265b2c2bf0f4ed68f3a8a75e
Fontaine Cook <fontaine cook@pearance.com>
2013-05-30 22:35:39 -0400

    Add resources page

--- pages/resources.php
+++ pages/resources.php
@@ -1,9 +1,16 @@
-<?php include('../includes/header.php'); ?>
-<?php include('../includes/nav.php'); ?>
+				<?php include('../includes/header.php'); ?>
+				<?php include('../includes/nav.php'); ?>
 
-<ul>
-	<li><a href="http://www.freeformatter.com/html-escape.html#ad-output">Free Formating Tools for Developers</a></li>
-	<li><a href="http://twitter.github.io/bootstrap/index.html">Twitter's Bootstrap</a></li>
-</ul>
+				<h2>Tools and Frameworks</h2>
+				<ul>
+					<li><a href="http://www.freeformatter.com/html-escape.html#ad-output">Free Formating Tools for Developers</a></li>
+					<li><a href="http://twitter.github.io/bootstrap/index.html">Twitter's Bootstrap</a></li>
+				</ul>
 
-<?php include('../includes/footer.php'); ?>
+				<h2>References</h2>
+				<ul>
+					<li><a href="http://www.w3.org/TR/html5/">W3C HTML5 Spec</a></li>
+					<li><a href="http://gsnedders.html5.org/outliner/">Semantic Structure Outliner</a></li>
+				</ul>
+
+				<?php include('../includes/footer.php'); ?>
