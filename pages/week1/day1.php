3f50d66339d69de00983a05640baf5247f5ea735
Fontaine Cook <fontaine cook@pearance.com>
2013-06-01 19:43:47 -0400

    Edited Semantic HTML and CSS Preprocessor sections

--- pages/week1/day1.php
+++ pages/week1/day1.php
@@ -1,38 +1,7 @@
 				<?php include('../../includes/header.php'); ?>
 				<?php include('../../includes/nav.php'); ?>
-								<!-- SIDEBAR NAVIGATION -->
 
-								<!-- <nav id="toc" class="span3 bs&#45;docs&#45;sidebar"> -->
-								<!-- 	<ul class="nav nav&#45;list bs&#45;docs&#45;sidenav"> -->
-								<!-- 		<li class="nav&#45;header">Table of Contents</li> -->
-								<!-- 		<li class="active"><a href="#">Home</a></li> -->
-								<!-- 		<li><a href="#career&#45;opportunities"><i class="icon&#45;chevron&#45;right"></i>Career Opportunities</a></li> -->
-								<!-- 		<li><a href="#domain&#45;names"><i class="icon&#45;chevron&#45;right"></i>Choosing Domain Names</a></li> -->
-								<!-- 		<li><a href="#history&#45;internet"><i class="icon&#45;chevron&#45;right"></i>History of the Internet</a></li> -->
-								<!-- 		<li><a href="#how&#45;internet&#45;works"><i class="icon&#45;chevron&#45;right"></i>How the Internet works?</a></li> -->
-								<!-- 		<li><a href="#internet&#45;protocols"><i class="icon&#45;chevron&#45;right"></i>Internet Protocols</a></li> -->
-								<!-- 		<li><a href="#industry&#45;terminology"><i class="icon&#45;chevron&#45;right"></i>Web Industry Terminology</a></li> -->
-								<!-- 		<li><a href="#web&#45;dev&#45;apps"><i class="icon&#45;chevron&#45;right"></i>Popular Web Development Apps</a></li> -->
-								<!-- 		<li><a href="#x&#45;browser&#45;compatibility"><i class="icon&#45;chevron&#45;right"></i>Cross Browser Compatibility</a></li> -->
-								<!-- 		<li><a href="#dir&#45;structure"><i class="icon&#45;chevron&#45;right"></i>Directory Structure</a></li> -->
-								<!-- 		<li><a href="#html&#45;intro"><i class="icon&#45;chevron&#45;right"></i>Introduction to HTML</a></li> -->
-
-								<!-- 		<li class="nav&#45;header">Assignments</li> -->
-								<!-- 		<li><a href="#your&#45;resume">Your Resume</a></li> -->
-								<!-- 		<li><a href="#daily&#45;terms">Study Terms of the Day</a></li> -->
-								<!-- 		<li><a href="#independent&#45;review">Independent Review</a></li> -->
-								<!-- 	</ul> -->
-
-								<!-- 	<br> -->
-								<!-- 	<p> -->
-								<!-- 		<span class="label label&#45;important">Note:</span> -->
-								<!-- 		consider site design in photoshop class -->
-								<!-- 	</p> -->
-								<!-- </nav> -->
-
-
-
-				<?php # MAIN CONTENT HEADING ?>
+				<?php # MAIN CONTENT HEADING ?><!--{-->
 				<div id="main-content" class="span12">
 					<div class="hero-unit">
 						<h1>Frontend Basics</h1>
@@ -40,21 +9,25 @@
 					</div>
 
 					<div class="accordion" id="accordion">
-
-						<?php # CAREER OPPORTUNITIES ?>
-						<section id="career-opportunities" class="accordion-group">
+<!--}-->
+						<?php # CAREER OPPORTUNITIES ?><!--{-->
+						<section id="career-opportunities" class="accordion-group"><!--{-->
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#career-opportunities-c">
 								<h2>Career Opportunities</h2>
 							</a>
-
-							<div id="career-opportunities-c" class="accordion-body collapse">
+<!--}-->
+							<div id="career-opportunities-c" class="accordion-body collapse"><!--{-->
 								<div class="accordion-inner">
 									<p>Here are some example job postings that illustrate the kind of skill sets the industry is looking for:</p>
-									<p><span class="label label-warning">Playing Catch Up?</span> Review the job postings below and observe the particularly the highlighted portions to get a feel for what skill sets and technologies the industry is looking for.</p>
+									<p>
+										<span class="label label-warning">Playing Catch Up?</span>
+										Review the job postings below and observe particularly the highlighted portions
+										to get a feel for what skill sets and technology stacks the industry is looking for.
+									</p>
 									<br>
 
 									<ul class="thumbnails">
-										<li class="span3">
+										<li class="span4">
 											<div class="thumbnail">
 												<img src="../../img/modus.jpg" data-src="holder.js/300x200" alt="Modus Create">
 												<h3>Modus Create</h3>
@@ -63,7 +36,7 @@
 											</div>
 										</li>
 
-										<li class="span3">
+										<li class="span4">
 											<div class="thumbnail">
 												<img src="../../img/menlo.jpg" data-src="holder.js/300x200" alt="Menlo Technologies">
 												<h3>Menlo Tech</h3>
@@ -72,7 +45,7 @@
 											</div>
 										</li>
 
-										<li class="span3">
+										<li class="span4">
 											<div class="thumbnail">
 												<img src="../../img/rht.jpg" data-src="holder.js/300x200" alt="Robert Half Technology">
 												<h3>Robert Half Tech</h3>
@@ -81,12 +54,48 @@
 											</div>
 										</li>
 									</ul>
+									<!--}-->
+									<h3>Semantic HTML</h3><!--{-->
+									<p>
+										Semantic HTML means you use
+										<abbr title="Hyper Text Markup Language">HTML</abbr> to
+										markup your content strictly for what it <em>means</em>. Not
+										for how you want it to <em>look</em>. The latter would be
+										a job for <abbr title="Cascading Style Sheets">CSS</abbr>
+										only. This topic will be covered in-depth in this course.
+									</p>
 
-									<h3 class="fyi">HTML and Semantics</h3>
-									<h3 class="fyi">CSS and Popular Preprocessors</h3>
+
+
+
+<!--}-->
+									<h3 class="fyi">CSS and Popular Preprocessors</h3><!--{-->
+									<p>
+										CSS preprocessors take code written in a more feature rich preprocessed language (Such as:
+										<a href="http://sass-lang.com/" target="_blank">Sass</a>,
+										<a href="http://lesscss.org/" target="_blank">LESS</a>, or
+										<a href="http://learnboost.github.com/stylus/" target="_blank">Stylus</a>) and then
+										processes it into basic CSS. One way to think of them is, as some sort of booster add-on that turns
+										basic CSS into advance CSS. This course will cover basic CSS. Afterwards, you are encourage to
+										familiarize yourself in one of the preprocessor languages.
+									</p>
+
+									<p class="recommendation well well-small">
+										<span class="label label-info"><i class="icon-thumbs-up icon-white"></i> Recommended:</span>
+										<a href="http://sass-lang.com/" target="_blank">Sass</a> is emerging as one of the most popular
+										preprocessors due largely in-part by the rapidly growing popularity of the
+										<a href="http://compass-style.org/" target="_blank">Compass CSS Framework</a>.
+									</p>
+
+									<div style="text-align: center;"><img src="../../img/sass-compass.jpg" alt="sass+compass"></div>
+									<!--}-->
+
 									<h3 class="fyi">JavaScript &amp; Popular Frameworks</h3>
+
 									<h3 class="fyi">Responsive Design</h3>
+
 									<h3 class="fyi">Version Control System</h3>
+
 									<h4>What is VCS?</h4>
 									<p class="lead">
 										Version Control System (VCS) also known as revision or source control is an aspect of source control management (SCM) is software that allows a team to manage changes of documents, programs, images and other information stored in computer files. Changes are usually identified by an incrementing number or letter code also known as revision number or revision.
@@ -133,11 +142,14 @@
 									</ul>
 									<p class="well well-large">Git is a free and open source distributed VCS that is rapidly emerging as the industry defacto standard in part by the growing popularity of <a href="https://github.com/">GitHub</a>.</p>
 
-									<a class="btn" href="http://try.github.io/levels/1/challenges/1">
-										Learn Git in your browser for free with<img src="../../img/try-git.png" alt="" width="64">
+									Learn Git in your browser for free with
+									<a class="" href="http://try.github.io/levels/1/challenges/1">
+										<img src="../../img/try-git.png" alt="" width="64">
 									</a>
 									<h3 class="fyi">Command Line Interfaces</h3>
+
 									<h3 class="fyi">Agile Methodology</h3>
+
 									<h3 class="fyi">Industry Social Sites</h3>
 									<h3>Popular Web Development Apps</h3>
 								</div><!-- accordion-inner -->
@@ -145,8 +157,8 @@
 						</section>
 
 
-
-						<?php # DOMAIN NAMES ?>
+<!--}-->
+						<?php # DOMAIN NAMES ?><!--{-->
 						<section id="domain-names" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#domain-names-c">
 								<h2>Choosing Domain Names</h2>
@@ -155,10 +167,10 @@
 							<div id="domain-names-c" class="accordion-body collapse">
 								<div class="accordion-inner">
 
-									<div class="tip well well-small">
+									<p class="tip well well-small">
 										<span class="label label-success"><i class="icon-thumbs-up icon-white"></i> Tip:</span>
 										Keep the following attributes in mind when choosing your domain name:
-									</div>
+									</p>
 
 									<ol>
 										<li>Short - Make it as long as it needs to be, but no longer.</li>
@@ -177,8 +189,8 @@
 						</section>
 
 
-
-						<?php # HISTORY OF THE INTERNET ?>
+<!--}-->
+						<?php # HISTORY OF THE INTERNET ?><!--{-->
 						<section id="history-internet" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#history-internet-c">
 								<h2>History of the Internet</h2>
@@ -193,8 +205,8 @@
 						</section>
 
 
-
-						<?php # HOW THE INTERNET WORKS ?>
+<!--}-->
+						<?php # HOW THE INTERNET WORKS ?><!--{-->
 						<section id="how-internet-works" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#how-internet-works-c">
 								<h2>How the Internet Works</h2>
@@ -208,8 +220,8 @@
 						</section>
 
 
-
-						<?php # CROSS BROWSER COMPATIBILITY ?>
+<!--}-->
+						<?php # CROSS BROWSER COMPATIBILITY ?><!--{-->
 						<section id="x-browser-compatibility" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#x-browser-compatibility-c">
 								<h2>Cross Broswer Compatibility</h2>
@@ -224,24 +236,26 @@
 						</section>
 
 
-
-						<?php # FILE & DIRECTORY CONVENTIONS ?>
+<!--}-->
+						<?php # FILE AND DIRECTORY CONVENTIONS ?><!--{-->
 						<section id="file-dir-conventions" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#file-dir-conventions-c">
-								<h2>File &amp; Directory Conventions</h2>
+								<h2>File and Directory Conventions</h2>
 							</a>
 
 							<div id="file-dir-conventions" class="accordion-body collapse">
 								<div class="accordion-inner">
 									<h3>Directory Structure</h3>
 									<h3>File and Folder Names</h3>
+									<h4>Naming Files and Directories</h4>
+									<p>Case sensitivity</p>
 								</div><!-- accordion-inner -->
 							</div>
 						</section>
 
 
-
-						<?php # INTRODUCTION TO HTML ?>
+<!--}-->
+						<?php # INTRODUCTION TO HTML ?><!--{-->
 						<section id="html-intro" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#html-intro-c">
 								<h2>Introduction to HTML</h2>
@@ -261,8 +275,9 @@
 										<li>The index finger handles its home key, the key above it and the key below V. In addition, the index finger handles the keys bordering the wall" of the box. For instance, the left index handles its home key (F), the key above it (R), the key below it (V) and the keys	vbordering the ridges, T G and B.</li>
 									</ul>
 
-									<h4>Naming Files and Directories</h4>
-									<p>Case sensitivity</p>
+									<h4>Attributes</h4>
+									<p>HTML tags can contain one or more attributes. Attributes are added to a tag to provide the browser with more information about how the tag should appear or behave. Attributes consist of a name and a value separated by an equals (=) sign, with the value surrounded by double quotes. Here's an example, style="color:black;".</p>
+
 
 
 <!-- http://www.w3schools.com/html/html_intro.asp -->
@@ -308,8 +323,8 @@
 
 
 
-
-						<?php # ASSIGNMENTS ?>
+<!--}-->
+						<?php # ASSIGNMENTS ?><!--{-->
 						<section id="assignments" class="accordion-group">
 							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#assignments-c">
 								<h2>Assignments</h2>
@@ -328,10 +343,10 @@
 										Ensure you include all the applicable sections below:
 									</p>
 
-									<div class="tip well well-small">
+									<p class="tip well well-small">
 										<span class="label label-success"><i class="icon-thumbs-up icon-white"></i> Tip:</span>
 										Focus on content, not formating... for now.
-									</div>
+									</p>
 
 									<ul>
 								  	<li>Education</li>
@@ -350,8 +365,8 @@
 						</section>
 
 
-
-						<?php # SUBJECT SECTION (TEMPLATE) ?>
+<!--}-->
+						<?php # SUBJECT SECTION (TEMPLATE) ?><!--{-->
 						<!-- <section id="" class="accordion&#45;group"> -->
 						<!-- 	<a class="accordion&#45;toggle" data&#45;toggle="collapse" data&#45;parent="#accordion" href="#idHere"> -->
 						<!-- 		<h2>Subject Title (Template)</h2> -->
@@ -365,9 +380,12 @@
 						<!-- </section> -->
 
 
-
-				<?php # MAIN CONTENT FOOTER ?>
+<!--}-->
+				<?php # MAIN CONTENT FOOTER ?><!--{-->
 					</div><!-- accordion -->
 
-				</div><!--main-content-->
+				</div><!--main-content--><!--}-->
 				<?php include('../../includes/footer.php'); ?>
+				<!--
+					vim: fdm=marker:fmr={,}:ft=html:tw=120:
+				-->
