eca9a39ab0b46767206da9ddb83711cc769e124b
Fontaine Cook <fontaine cook@pearance.com>
2013-06-01 21:05:29 -0400

    Add scratch file for possibly reusable data

---
+++ archive/scratch.php
@@ -0,0 +1,33 @@
+
+<!-- SIDEBAR NAVIGATION -->
+
+<nav id="toc" class="span3 bs-docs-sidebar">
+	<ul class="nav nav-list bs-docs-sidenav">
+		<li class="nav-header">Table of Contents</li>
+		<li class="active"><a href="#">Home</a></li>
+		<li><a href="#career-opportunities"><i class="icon-chevron-right"></i>Career Opportunities</a></li>
+		<li><a href="#domain-names"><i class="icon-chevron-right"></i>Choosing Domain Names</a></li>
+		<li><a href="#history-internet"><i class="icon-chevron-right"></i>History of the Internet</a></li>
+		<li><a href="#how-internet-works"><i class="icon-chevron-right"></i>How the Internet works?</a></li>
+		<li><a href="#internet-protocols"><i class="icon-chevron-right"></i>Internet Protocols</a></li>
+		<li><a href="#industry-terminology"><i class="icon-chevron-right"></i>Web Industry Terminology</a></li>
+		<li><a href="#web-dev-apps"><i class="icon-chevron-right"></i>Popular Web Development Apps</a></li>
+		<li><a href="#x-browser-compatibility"><i class="icon-chevron-right"></i>Cross Browser Compatibility</a></li>
+		<li><a href="#dir-structure"><i class="icon-chevron-right"></i>Directory Structure</a></li>
+		<li><a href="#html-intro"><i class="icon-chevron-right"></i>Introduction to HTML</a></li>
+
+		<li class="nav-header">Assignments</li>
+		<li><a href="#your-resume">Your Resume</a></li>
+		<li><a href="#daily-terms">Study Terms of the Day</a></li>
+		<li><a href="#independent-review">Independent Review</a></li>
+	</ul>
+
+	<br>
+	<p>
+		<span class="label label-important">Note:</span>
+		consider site design in photoshop class
+	</p>
+</nav>
+
+
+
